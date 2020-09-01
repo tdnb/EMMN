9dab05bb2669488f3fcd613f31bf043850b65dd4
tadanobu <public@tadanobu.com>
2020-09-01 15:09:37 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,149 @@
+<h1 align="center">EMMN</h1>
+<h5 align="center">Exponential Mean-centric Moving Normalization for Optimization Algorithm</h5>
+
+<div align="center"><a href="https://arxiv.org/abs/"> arxiv.org/abs/</a></div>
+
+
+working in progress.
+
+- [Introduction](#Introduction)
+- [Motivation](#Motivation)
+- [SLSBoost](#SLSBoost])
+- [Quick Start Guide](#quick-start-guide)
+
+- [Citation](#citation)
+
+
+
+## Introduction
+
+EMMN is a new stochastic gradient-based optimization algorithm designed to provide the speed of adaptive methods while maintaining the generalization ability of non adaptive methods.
+
+In this algorithm, we first decompose the gradient into two parts, one is the momentum component and the other is the residual component (i.e. gradient - momentum). Then, the residual component is normalized by the moving estimation of the standard deviation. Finally, these two components are added together and is used to update parameters. This is illustrated bellow.
+
+<p align="center"><img width="100%" src="img_readme/figure1_outline.svg"/></p>
+
+**Figure 1 : Visualization of EMMA algorithm.**
+
+Since this algorithm normalize gradient as if the point of the estimated moving average (i.e. momentum) is the center (i.e. zero), momentum is not affected by the normalization. Hence, momentum is invariant. This explicitly mitigate the problem of *diagonal correlated trajectory*, a latent property of adaptive optimization algorithm, which is identified and explained in [the paper](). Since gradient is divided by running estimated standard deviation at each time step, the fluctuation of the parameter moving has the similar radius from the estimated center for each element. This enables *soft-homoscedastic gradient sampling* which brings informative momentum estimation.
+
+
+
+## Motivation
+
+In this study, we theoretically derived the following two conjectures as the properties of the adaptive optimization algorithms.
+
+- Diagonal correlated trajectory
+- Soft-homoscedastic gradient sampling
+
+We empirically showed the existence of these two properties,  as shown in Figure 2 for diagonal correlated trajectory, Figure 5 and table 2 for soft-homoscedastic gradient sampling.
+
+
+<img src="img_readme/figure2.png"/>
+
+<img src="img_readme/figure5.png"/>
+
+<!--We empirically showed the existence of these two properties,  as shown in Figure 2 for diagonal correlated trajectory, Figure 3 and table 1 for soft-homoscedastic gradient sampling.
+
+<img src="img_readme/figure2.svg"/>
+
+**Figure 2 : PDC scores.** PDC score of RMSprop, Adam, Adagrad, RAdam, EMMN, SGD and  SGDm (SGD with momentum) on training CIFAR-100. Learning rate was decayed around 12k and 16k. Savitzky-Golay filter with window size 41 order of polynomial 3 was used to smooth the graph, since the raw data has high variance.
+
+
+<div style="display: flex">
+<table style="width: 48%; margin-right:2%">
+  <tr>
+    <th>Algorithm</th>
+    <th>variance of element-wise variance</th>
+  </tr>
+  <tr>
+    <td>SGD momentum</td>
+    <td>15.96</td>
+  </tr>
+  <tr>
+    <td>RAdam</td>
+    <td>5.61</td>
+  </tr>
+  <tr>
+    <td>EMMN</td>
+      <td><strong>1.54</strong></td>
+  </tr>
+</table>
+<div style="width: 48%; margin-left:2%">
+    <strong>Table 1: Comparison of variance of parameter-wise variance of ten continuous timestep of parameter's data points.</strong> Data points are scaled so that the volume of the hyperrectangle defined by planes of std=1 is 1. Then, compute variance element-wise and finally compute variance of them.
+</div>
+</div>
+
+<div style="display: flex">
+    <div style="width: 31%; margin-left:2.3%"><strong>a.</strong> SGD momentum</div>
+    <div style="width: 31%; margin-left:2.3%"><strong>b.</strong> RAdam</div>
+    <div style="width: 31%; margin-left:2.3%"><strong>c.</strong> EMMN</div>
+</div>
+<div style="display: flex">
+    <img src="img_readme/figure5_sgd.png" style="width: 31%; margin-left:2.3" />
+    <img src="img_readme/figure5_radam.png"  style="width: 31%; margin-left:2.3%" />
+    <img src="img_readme/figure5_emmn.png"  style="width: 31%; margin-left:2.3%" />
+</div>
+**Figure 3: 3D projected parameter sampling.** Ten continuous timestep parameter's data points are projected to the three dimensional space to visualize dimension-wise variance for SGD momentum, RAdam and EMMN. Momentum coefficient (i.e. discount rate for exponential moving average or momentum computation) are set as 0.95 on all algorithm for fair comparison. Also, these plots are scaled so that the volume of the box defined by planes where standard deviation is 1 are the same for all of algorithm.  **a.** SGD momentum produces big difference of element-wise variance. **b.** RAdam successfully compress variance differences. We chose RAdam instead of Adam because of its robustness for parameter setting. **c.** Our proposal optimizer, EMMN, compress variance differences most strongly.-->
+
+Our study suggest diagonal correlated trajectory causes high-variance, thus lower generalization ability as show in Figure 4. We theoretically show soft-homoscedastic gradient sampling has advantage in the paper. Please refer [the paper]() for the detail.
+
+<img src="img_readme/figure4.png"/>
+
+*Our motivation is to cancel  the diagonal correlation of parameters trajectory while keeping soft-homoscedastic gradient sampling.*
+
+
+
+## SLSBoost
+
+SLSBoost is sub-algorithm for gradient-based optimization algorithm to speed up the learning process.
+
+Based on our studies on the trajectories of the learning parameters, we hypothesize that high diagonal correlations of the trajectories of the learning parameters lead to higher straight line stability of the trajectories, and that high straight line stability is a direct cause of the improvement in learning speed.
+
+Our experiments have shown that by increasing straight line stability without increasing diagonal correlation, it is possible to improve training speed while minimizing generalized performance degradation.
+
+*SLSBoost algorithm boosts straight line stability of the parameter trajectories without increasing diagonal correlation of the trajectories.* The algorithm is implemented experimentally in this pytorch implementation of the EMMN.
+
+This section is a work in progress.
+
+
+
+## Quick Start Guide
+
+EMMN can be easily replace from SGD momentum by converting learning rate from SGD to EMMN using the bellow equation.
+
+<!-- $$
+\frac{\alpha_{sgd}}{(1 - \beta_{sgd})} = \alpha_{emmn}
+$$ --> 
+
+<div align="center"><img src="img_readme/SWFtLQee6l.svg"/></div>
+
+Where α is leraning rate, βsgd is momentum factor of SGD. For example, if βsgd = 0.95 and αsgd = 0.15, learning rate of EMMN is:
+
+<!-- $$
+\alpha_{emmn} = \frac{\alpha_{sgd}}{1 - \beta_{sgd}} = \frac{0.15}{1 - 0.95} = 3
+$$ --> 
+
+<div align="center"><img src="img_readme/GbDEATPdku.svg"/></div>
+
+We recommend setting β of EMMN to 0.95 for the first time, and then adjusting it between 0.9 and 0.99 if necessary.
+
+If you need to further speed up the learning process, try SLSBoost. We  haven't fully experimented with it yet, but for now we would recommend something around sls_boost=(0.05, 0.995) or  (0.3, 0.95) or in between.
+
+
+
+## Citation
+
+If you find our algorithm useful, please cite the following paper.
+
+> Tadanobu Ohata (2020). EMMN: Exponential Mean-centric Moving Normalization for Stochastic Optimization
+
+```
+@article{ohata2020emmn,
+  title={EMMN: Exponential Mean-centric Moving Normalization for Stochastic Optimization},
+  author={Tadanobu Ohata},
+  journal={arXiv preprint arXiv:},
+  year={2020}
+}
+```
+
---
+++ emmn/emmn.py
@@ -0,0 +1,243 @@
+import torch
+from torch.optim.optimizer import Optimizer
+
+
+class EMMN(Optimizer):
+    u"""Implements EMMN algorithm with SLSBoost algorithm
+
+    Arguments:
+        params (iterable): iterable of parameters to optimize or dicts defining parameter groups
+        lr (float, optional): learning rate (default: 1)
+        beta (float, optional): discount factor of EMA (default: 0.95)
+        sigma(float | tuple): setting for target standard deviation. (default: (0.3, 0.99))
+            | float: fixed target standard deviation will be used for normalized residual gradient (recommended value = 2e-4)
+            | tuple(float, float): relative target standard deviation will be used. (λ, β_v) of λ√EMA(mean(Var(θ'))| β_v))
+        eps (float, optional): term added to the denominator to improve numerical stability (default: 1e-8)
+        weight_decay (float, optional): factor of one step weight decay (i.e. one_step_weight_decay = weight_decay * lr * alpha) (default: 0)
+        sls_boost (tuple:(float, float) | None, optional): (mixing ratio of boosting vector, discount factor of EMA(updater)) (default: None)
+    """
+
+    def __init__(self, params, lr=1, beta=0.95, sigma=(0.3, 0.99), eps=1e-8, weight_decay=0, sls_boost=None):
+        if lr <= 0.0:
+            raise ValueError("Argument lr must be 0.0 < lr, but lr={}".format(lr))
+        if not 0.0 <= beta <= 1.0:
+            raise ValueError("Argument beta must be 0.0 <= beta <= 1.0, but beta={}".format(beta))
+        if not (isinstance(sigma, float) or (isinstance(sigma, tuple) and len(sigma) == 2)):
+            raise ValueError("Argument sigma must be float or tuple of (float, float), but type(sigma)={}".format(type(sigma)))
+        if isinstance(sigma, float) and sigma <= 0.0:
+            raise ValueError("Argument sigma must be 0.0 < sigma, but sigma={}".format(sigma))
+        if isinstance(sigma, tuple) and sigma[0] <= 0.0:
+            raise ValueError("Argument sigma[0] must be 0.0 < sigma[0], but sigma[0]={}".format(sigma[0]))
+        if isinstance(sigma, tuple) and not 0.0 <= sigma[1] <= 1.0:
+            raise ValueError("Argument sigma[1] must be 0.0 <= sigma[1] <= 1.0, but sigma[1]={}".format(sigma[1]))
+        if eps <= 0.0:
+            raise ValueError("Argument eps must be 0.0 < eps, but eps={}".format(eps))
+        if weight_decay < 0:
+            raise ValueError("Argument weight_decay must be 0.0 <= weight_decay, but weight_decay={}".format(weight_decay))
+        if sls_boost is not None:
+            if not 0.0 <= sls_boost[0] <= 1.0:
+                raise ValueError("Argument sls_boost[0] must be 0.0 <= sls_boost[0] <= 1.0, but sls_boost[0]={}".format(sls_boost[0]))
+            if not 0.0 <= sls_boost[1] <= 1.0:
+                raise ValueError("Argument sls_boost[1] must be 0.0 <= sls_boost[1] <= 1.0, but sls_boost[1]={}".format(sls_boost[1]))
+
+        defaults = dict(lr=lr, beta=beta, sigma=sigma, eps=eps, weight_decay=weight_decay, sls_boost=sls_boost)
+        super(EMMN, self).__init__(params, defaults)
+
+    def step(self, closure=None):
+        """Performs a single optimization step.
+
+        Arguments:
+            closure (callable, optional): A closure that reevaluates the model
+                and returns the loss.
+        """
+        loss = None
+        if closure is not None:
+            loss = closure()
+
+        for group in self.param_groups:
+            for p in group['params']:
+                if p.grad is None:
+                    continue
+                grad = p.grad.data
+                if grad.is_sparse:
+                    raise RuntimeError('EMMN does not support sparse gradients')
+                state = self.state[p]
+
+                if isinstance(group['sigma'], float):
+                    sigma = group['sigma']
+                    mode = 'fix'
+                else:
+                    sigma = group['sigma'][0]
+                    beta_var = group['sigma'][1]
+                    mode = 'relative'
+                beta = group['beta']
+                sls_boost = group['sls_boost']
+
+                if len(state) == 0:
+                    state['step'] = 0
+                    state['ema_grad'] = torch.zeros_like(p.data)
+                    state['ema_sqr_grad'] = torch.zeros_like(p.data)
+                    if mode == 'relative':
+                        state['ema_mean_var'] = torch.zeros(1).to(p.data.device)
+
+                state['step'] += 1
+                step = state['step']
+                ema_grad = state['ema_grad']
+                ema_sqr_grad = state['ema_sqr_grad']
+                ema_grad.mul_(beta).add_(1 - beta, grad)
+                ema_sqr_grad.mul_(beta).addcmul_(1 - beta, grad, grad)
+                var_grad = ema_sqr_grad.addcmul(-1, ema_grad, ema_grad)
+                std_grad = var_grad.sqrt().add(group['eps'])
+                grad_adjusted = grad * (1 - beta**step)
+                if step == 1:
+                    emma_grad = grad_adjusted
+                else:
+                    residual = grad_adjusted - ema_grad
+
+                    if mode == 'relative':
+                        ema_mean_var = state['ema_mean_var']
+                        ema_mean_var.mul_(beta_var).add_(1 - beta_var, var_grad.mean())
+                        ema_mean_var_bc = ema_mean_var / (1 - beta_var**(step-1))
+                        residual *= sigma * ema_mean_var_bc.sqrt() / std_grad
+                    else: # if mode == 'fix'
+                        residual *= sigma / std_grad
+                    emma_grad = ema_grad + residual
+
+                    if sls_boost is not None:
+                        if 'ema_u' not in state:
+                            ema_u = state['ema_u'] = torch.clone(emma_grad).detach() * (1 - sls_boost[1])
+                        else:
+                            ema_u = state['ema_u']
+                        target_vec = ema_u * emma_grad.norm() / ema_u.norm()
+                        boost_vec = sls_boost[0] * (target_vec - emma_grad)
+                        updater = emma_grad + boost_vec
+                        # updater *= emma_grad.norm() / updater.norm()
+                        ema_u.mul_(sls_boost[1]).add_(updater, alpha=1 - sls_boost[1])
+                        emma_grad = updater
+
+                if group['weight_decay'] != 0:
+                    p.data.mul_(1 - group['lr'] * group['weight_decay'])
+                p.data.add_(-group['lr'], emma_grad)
+
+        return loss
+
+
+class EMMN_PDCBoost(Optimizer):
+    u"""Implements EMMN algorithm with PDCBoost algorithm
+
+    Arguments:
+        params (iterable): iterable of parameters to optimize or dicts defining parameter groups
+        lr (float, optional): learning rate (default: 1)
+        beta (float, optional): discount factor of EMA (default: 0.95)
+        sigma(float | tuple): setting for target standard deviation. (default: (0.3, 0.99))
+            | float: fixed target standard deviation will be used for normalized residual gradient (recommended value = 2e-4)
+            | tuple(float, float): relative target standard deviation will be used. (λ, β_v) of λ√EMA(mean(Var(θ'))| β_v))
+        eps (float, optional): term added to the denominator to improve numerical stability (default: 1e-8)
+        weight_decay (float, optional): factor of one step weight decay (i.e. one_step_weight_decay = weight_decay * lr * alpha) (default: 0)
+        pdc_boost (tuple:(float, float) | None, optional): (mixing ratio of boosting vector, discount factor of EMA(updater)) (default: None)
+    """
+
+    def __init__(self, params, lr=1, beta=0.95, sigma=(0.3, 0.99), eps=1e-8, weight_decay=0, pdc_boost=None):
+        if lr <= 0.0:
+            raise ValueError("Argument lr must be 0.0 < lr, but lr={}".format(lr))
+        if not 0.0 <= beta <= 1.0:
+            raise ValueError("Argument beta must be 0.0 <= beta <= 1.0, but beta={}".format(beta))
+        if not (isinstance(sigma, float) or (isinstance(sigma, tuple) and len(sigma) == 2)):
+            raise ValueError("Argument sigma must be float or tuple of (float, float), but type(sigma)={}".format(type(sigma)))
+        if isinstance(sigma, float) and sigma <= 0.0:
+            raise ValueError("Argument sigma must be 0.0 < sigma, but sigma={}".format(sigma))
+        if isinstance(sigma, tuple) and sigma[0] <= 0.0:
+            raise ValueError("Argument sigma[0] must be 0.0 < sigma[0], but sigma[0]={}".format(sigma[0]))
+        if isinstance(sigma, tuple) and not 0.0 <= sigma[1] <= 1.0:
+            raise ValueError("Argument sigma[1] must be 0.0 <= sigma[1] <= 1.0, but sigma[1]={}".format(sigma[1]))
+        if eps <= 0.0:
+            raise ValueError("Argument eps must be 0.0 < eps, but eps={}".format(eps))
+        if weight_decay < 0:
+            raise ValueError("Argument weight_decay must be 0.0 <= weight_decay, but weight_decay={}".format(weight_decay))
+        if pdc_boost is not None:
+            if not 0.0 <= pdc_boost[0] <= 1.0:
+                raise ValueError("Argument pdc_boost[0] must be 0.0 <= pdc_boost[0] <= 1.0, but pdc_boost[0]={}".format(pdc_boost[0]))
+            if not 0.0 <= pdc_boost[1] <= 1.0:
+                raise ValueError("Argument pdc_boost[1] must be 0.0 <= pdc_boost[1] <= 1.0, but pdc_boost[1]={}".format(pdc_boost[1]))
+
+        defaults = dict(lr=lr, beta=beta, sigma=sigma, eps=eps, weight_decay=weight_decay, pdc_boost=pdc_boost)
+        super(EMMN_PDCBoost, self).__init__(params, defaults)
+
+    def step(self, closure=None):
+        """Performs a single optimization step.
+
+        Arguments:
+            closure (callable, optional): A closure that reevaluates the model
+                and returns the loss.
+        """
+        loss = None
+        if closure is not None:
+            loss = closure()
+
+        for group in self.param_groups:
+            for p in group['params']:
+                if p.grad is None:
+                    continue
+                grad = p.grad.data
+                if grad.is_sparse:
+                    raise RuntimeError('EMMN does not support sparse gradients')
+                state = self.state[p]
+
+                if isinstance(group['sigma'], float):
+                    sigma = group['sigma']
+                    mode = 'fix'
+                else:
+                    sigma = group['sigma'][0]
+                    beta_var = group['sigma'][1]
+                    mode = 'relative'
+                beta = group['beta']
+                pdc_boost = group['pdc_boost']
+
+                if len(state) == 0:
+                    state['step'] = 0
+                    state['ema_grad'] = torch.zeros_like(p.data)
+                    state['ema_sqr_grad'] = torch.zeros_like(p.data)
+                    if mode == 'relative':
+                        state['ema_mean_var'] = torch.zeros(1).to(p.data.device)
+
+                state['step'] += 1
+                step = state['step']
+                ema_grad = state['ema_grad']
+                ema_sqr_grad = state['ema_sqr_grad']
+                ema_grad.mul_(beta).add_(1 - beta, grad)
+                ema_sqr_grad.mul_(beta).addcmul_(1 - beta, grad, grad)
+                var_grad = ema_sqr_grad.addcmul(-1, ema_grad, ema_grad)
+                std_grad = var_grad.sqrt().add(group['eps'])
+                grad_adjusted = grad * (1 - beta**step)
+                if step == 1:
+                    emma_grad = grad_adjusted
+                else:
+                    residual = grad_adjusted - ema_grad
+
+                    if mode == 'relative':
+                        ema_mean_var = state['ema_mean_var']
+                        ema_mean_var.mul_(beta_var).add_(1 - beta_var, var_grad.mean())
+                        ema_mean_var_bc = ema_mean_var / (1 - beta_var**(step-1))
+                        residual *= sigma * ema_mean_var_bc.sqrt() / std_grad
+                    else: # if mode == 'fix'
+                        residual *= sigma / std_grad
+                    emma_grad = ema_grad + residual
+
+                    if pdc_boost is not None:
+                        if 'ema_u' not in state:
+                            ema_u = state['ema_u'] = torch.clone(emma_grad).detach() * (1 - pdc_boost[1])
+                        else:
+                            ema_u = state['ema_u']
+                        target_vec = ema_u.sign()
+                        target_vec *= emma_grad.norm() / target_vec.norm()
+                        boost_vec = pdc_boost[0] * (target_vec - emma_grad)
+                        updater = emma_grad + boost_vec
+                        # updater *= emma_grad.norm() / updater.norm()
+                        ema_u.mul_(pdc_boost[1]).add_(updater, alpha=1 - pdc_boost[1])
+                        emma_grad = updater
+
+                if group['weight_decay'] != 0:
+                    p.data.mul_(1 - group['lr'] * group['weight_decay'])
+                p.data.add_(-group['lr'], emma_grad)
+
+        return loss
